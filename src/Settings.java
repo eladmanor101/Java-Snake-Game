db53546749595f92b37656919569ba235e8685c2
Elad <eladmanor101@gmail.com>
2021-09-11 18:19:12 +0300

    Settings class added.

---
+++ src/Settings.java
@@ -0,0 +1,10 @@
+public final class Settings
+{
+    static int boardWidth = 10, boardHeight = 10;
+    static int screenWidth = 800, screenHeight = 800;
+
+    private Settings()
+    {
+
+    }
+}
